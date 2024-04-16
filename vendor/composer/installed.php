81ab08135aac5ffab6bc54738e3087aa73f91c60
Wilquey <wilquey@gmail.com>
2024-04-16 20:40:12 -0300

    test localhost

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '971071c12f15fb7a775874cea3aaff02197f2408',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -389,9 +389,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '971071c12f15fb7a775874cea3aaff02197f2408',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
